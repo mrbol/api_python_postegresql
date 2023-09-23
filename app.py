1ba7e9b48afa99e5b4064fbc293b355d153483b6
Waldiney Romero <wrf.romero@gmail.com>
2023-09-23 01:16:42 -0300

    Usando SQLAlchemy com PostgreSQL

---
+++ app.py
@@ -0,0 +1,78 @@
+from flask_openapi3 import OpenAPI, Info, Tag
+from flask import redirect
+from urllib.parse import unquote
+
+from sqlalchemy.exc import IntegrityError
+
+from model import Session, Produto
+from schemas import *
+from flask_cors import CORS
+
+info = Info(title="Minha API", version="1.0.0")
+app = OpenAPI(__name__, info=info)
+CORS(app)
+
+# definindo tags
+home_tag = Tag(name="Documentação", description="Seleção de documentação: Swagger, Redoc ou RapiDoc")
+produto_tag = Tag(name="Produto", description="Adição e visualização de produtos à base")
+
+
+@app.get('/', tags=[home_tag])
+def home():
+    """Redireciona para /openapi, tela que permite a escolha do estilo de documentação.
+    """
+    return redirect('/openapi')
+
+@app.post('/produto', tags=[produto_tag],
+          responses={"201": ProdutoViewSchema, "409": ErrorSchema, "400": ErrorSchema})
+def add_produto(form: ProdutoSchema):
+    """Adiciona um novo Produto à base de dados
+
+    Retorna uma representação dos produtos e comentários associados.
+    """
+    produto = Produto(
+        nome=form.nome,
+        quantidade=form.quantidade,
+        valor=form.valor)
+
+    try:
+        # criando conexão com a base
+        session = Session()
+        # adicionando produto
+        session.add(produto)
+        # efetivando o camando de adição de novo item na tabela
+        session.commit()
+
+        return apresenta_produto(produto), 200
+
+    except IntegrityError as e:
+        # como a duplicidade do nome é a provável razão do IntegrityError
+        error_msg = "Produto de mesmo nome já salvo na base :/"
+
+        return {"mesage": error_msg}, 409
+
+    except Exception as e:
+        # caso um erro fora do previsto
+        error_msg = "Não foi possível salvar novo item :/"
+
+        return {"mesage": error_msg}, 400
+
+@app.get('/produtos', tags=[produto_tag],
+         responses={"200": ListagemProdutosSchema, "404": ErrorSchema})
+def get_produtos():
+    """Faz a busca por todos os Produto cadastrados
+
+    Retorna uma representação da listagem de produtos.
+    """
+    # criando conexão com a base
+    session = Session()
+    # fazendo a busca
+    produtos = session.query(Produto).all()
+
+    if not produtos:
+        # se não há produtos cadastrados
+        return {"produtos": []}, 200
+    else:
+        # retorna a representação de produto
+        print(produtos)
+        return apresenta_produtos(produtos), 200
---
+++ model/__init__.py
@@ -0,0 +1,26 @@
+from sqlalchemy_utils import database_exists, create_database
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+import os
+
+# importando os elementos definidos no modelo
+from model.base import Base
+from model.produto import Produto
+
+
+# url de acesso ao banco (essa é uma url de acesso ao postgresql local)
+# postgresql://usuario:senha@localhost/nome_do_banco
+db_url = "postgresql://postgres:123456@localhost/meu_teste"
+
+# cria a engine de conexão com o banco
+engine = create_engine(db_url, echo=False)
+
+# Instancia um criador de seção com o banco
+Session = sessionmaker(bind=engine)
+
+# cria o banco se ele não existir 
+if not database_exists(engine.url):
+    create_database(engine.url) 
+
+# cria as tabelas do banco, caso não existam
+Base.metadata.create_all(engine)
